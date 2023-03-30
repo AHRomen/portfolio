6576d00e05a26f9e6658ac0feafeef78ad2d03de
AHRomen <73189275+AHRomen@users.noreply.github.com>
2023-03-30 21:22:08 +0600

    Update custom.js

--- inc/js/custom.js
+++ inc/js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     
     AOS.init({
-      offset: 0 , // offset (in px) from the original trigger point
+      offset: 1000 , // offset (in px) from the original trigger point
       delay: 100, // values from 0 to 3000, with step 50ms
       duration: 3000
     });
